74cf00494064121269937df4efb4efd418b0cf65
JuTo_Hub <JustinTorres1@Outlook.com>
2020-10-01 23:52:14 -0500

    Fixed HTML and save button functions

--- Assets/logic.js
+++ Assets/logic.js
@@ -32,22 +32,21 @@ $(document).ready(function() {
 
     $(".saveBtn").click(function(event){
         event.preventDefault();
+
         var value = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
-        console.log(value);
-        console.log(time);
-        localStorage.setItem(value, time);
+        localStorage.setItem(time, value);
     })
 }
 saveData();
 })
 
 $("#9 .description").val(localStorage.getItem("9"));
-$("#10 .description").val(localStorage.getItem("10"));
-$("#11 .description").val(localStorage.getItem("11"));
-$("#12 .description").val(localStorage.getItem("12"));
-$("#13 .description").val(localStorage.getItem("13"));
-$("#14 .description").val(localStorage.getItem("14"));
-$("#15 .description").val(localStorage.getItem("15"));
-$("#16 .description").val(localStorage.getItem("16"));
-$("#17 .description").val(localStorage.getItem("17"));
+$("#10 .description").val(window.localStorage.getItem("10"));
+$("#11 .description").val(window.localStorage.getItem("11"));
+$("#12 .description").val(window.localStorage.getItem("12"));
+$("#13 .description").val(window.localStorage.getItem("13"));
+$("#14 .description").val(window.localStorage.getItem("14"));
+$("#15 .description").val(window.localStorage.getItem("15"));
+$("#16 .description").val(window.localStorage.getItem("16"));
+$("#17 .description").val(window.localStorage.getItem("17"));
